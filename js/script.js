3839a7d3f7e418a8a58cf4d4780aaaa865e7df89
Bob Landrey <robert.landrey@du.edu>
2015-09-30 03:51:29 -0600

    web upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+ 
+function runPhotochange() { 
+ document.getElementById('photochange').src='profile.jpg'; } 
+ window.setTimeout("runPhotochange()", 3000);  
+
+function show() {
+    AB = document.getElementById('captionload');
+    AB.style.display = 'inline';
+}
+setTimeout("show()", 5000);
+
